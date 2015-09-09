f089d6da76011a0c4c23776015865063aa7dfd38
James Reidy <jgr25@cornell.edu>
2015-09-09 10:52:16 -0400

    adding victoria02 changes

--- index.php
+++ index.php
@@ -17,16 +17,15 @@
 
 <div id="wrap">
   <div id="content-wrapper">
-  		
+
 	  	<div id="content">
         	<?php showImage(); ?>
-		    
+
             <?php include("inc/navigation.php") ?>
-            
-		    <hr /> 
-		
+
+		    <hr />
+
 		    <div id="main">
-            	
 		      	<p class="large">Under the sponsorship of The Division of Rare and Manuscript Collections, the Rose Goldsen Archive of New Media Art serves as a research repository of new media art and resources. The curatorial vision emphasizes digital interfaces and artistic experimentation by international, independent artists. Designed as an experimental center of research and creativity, the Goldsen Archive includes materials by individual artists and collaborates on conceptual experimentation and archival strategies with international curatorial and fellowship projects.</p>
 
                 <!-- <h2>Latest News</h2>
@@ -39,22 +38,22 @@
 
             <div id="thumbnails-wrap">
                 <div id="thumbnails-content">
-                    <a href="/special/renew.php" title="Mark Hansen and Ben Rubin, Listening Post (detail)" id="renew-link"><img src="images/hansenrubin_square.jpg" border="0" width="110" height="110" alt="Mark Hansen and Ben Rubin, Listening Post (detail)" /></a> 
-                    <a href="/general/index.php" title="Renate Ferro, Panic Hits Home (detail)" id="general-link"><img src="images/ferro_square.jpg" border="0" width="110" height="110" alt="Renate Ferro, Panic Hits Home (detail)" /></a> 
-                    <a href="/special/yao.php" title="Yao Jui Chung, The Soul Is But One Instant (detail)" id="yao-link"><img src="images/yao_square.jpg" border="0" width="110" height="110" alt="Yao Jui Chung, The Soul Is But One Instant (detail)" /></a> 
-                    <a href="/special/lynn.php" title="Lynn Hershman, Agent Ruby (detail)" id="hershman-link"><img src="images/hershman_square.jpg" border="0" width="110" height="110" alt="Lynn Hershman, Agent Ruby (detail)" /></a> 
-                    <a href="/general/interactive.php" title="Norie Neumark, Shock in the Ear (detail)" id="interactive-link"><img src="images/neumark_square.jpg" border="0" width="110" height="110" alt="Norie Neumark, Shock in the Ear (detail)" /></a> 
+                    <a href="/special/renew.php" title="Mark Hansen and Ben Rubin, Listening Post (detail)" id="renew-link"><img src="images/hansenrubin_square.jpg" border="0" width="110" height="110" alt="Mark Hansen and Ben Rubin, Listening Post (detail)" /></a>
+                    <a href="/general/index.php" title="Renate Ferro, Panic Hits Home (detail)" id="general-link"><img src="images/ferro_square.jpg" border="0" width="110" height="110" alt="Renate Ferro, Panic Hits Home (detail)" /></a>
+                    <a href="/special/yao.php" title="Yao Jui Chung, The Soul Is But One Instant (detail)" id="yao-link"><img src="images/yao_square.jpg" border="0" width="110" height="110" alt="Yao Jui Chung, The Soul Is But One Instant (detail)" /></a>
+                    <a href="/special/lynn.php" title="Lynn Hershman, Agent Ruby (detail)" id="hershman-link"><img src="images/hershman_square.jpg" border="0" width="110" height="110" alt="Lynn Hershman, Agent Ruby (detail)" /></a>
+                    <a href="/general/interactive.php" title="Norie Neumark, Shock in the Ear (detail)" id="interactive-link"><img src="images/neumark_square.jpg" border="0" width="110" height="110" alt="Norie Neumark, Shock in the Ear (detail)" /></a>
                     <a href="/general/cd.php" title="Reginald Woolery, Million Man March/World Wide Web (detail)" id="cd-link"><img src="images/woolery_square.jpg" border="0" width="110" height="110" alt="Reginald Woolery, Million Man March/World Wide Web (detail)" /></a>
                 </div>
-            </div>	
-            
+            </div>
+
             <?php include("inc/footer.php") ?>
-            
-			
-			
+
+
+
     	</div>  <!--close #content -->
 	</div>	<!--close #content-wrapper -->
-	
+
 <hr />
 
 
