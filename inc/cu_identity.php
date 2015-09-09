c10d88b46a1b87acd016440c40454e2fba8a8202
James Reidy <jreidy@cornell.edu>
2015-09-09 11:38:14 -0400

    replace search box with a link to search results page

--- inc/cu_identity.php
+++ inc/cu_identity.php
@@ -9,18 +9,6 @@
 	</div>
 
     <div id="search-form">
-        <script>
-          (function() {
-            var cx = '006762482862802971692:r-lchx86oz0';
-            var gcse = document.createElement('script');
-            gcse.type = 'text/javascript';
-            gcse.async = true;
-            gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-                '//cse.google.com/cse.js?cx=' + cx;
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(gcse, s);
-          })();
-        </script>
-        <gcse:search></gcse:search>
+        <a href="search_results.php">SEARCH</a>
     </div>
 </div>
