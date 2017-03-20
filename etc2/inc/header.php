e487ea965db50492a269c58adda04c051e750fdc
Melissa Wallace <mhk33@cornell.edu>
2017-03-20 17:56:09 -0400

    use php includes

---
+++ etc2/inc/header.php
@@ -0,0 +1,44 @@
+  <div id="skiptocontent"><a href="#maincontent">skip to main content</a></div>
+  <section class="cornell-identity">
+    <div class="container">
+      <div class="row">
+        <div class="cornell-brand">
+          <div class="cu-logo">
+            <img src="img/cul-logo-gray.gif">
+            <a id="insignia-link" href="http://www.cornell.edu/">Cornell Insignia</a>
+            <div class="unit-signature-links">
+              <a id="cornell-link" href="http://www.cornell.edu/">Cornell University</a>
+              <a id="unit-link" href="http://www.library.cornell.edu/">Cornell University Library</a>
+            </div>
+          </div>
+        </div>
+      </div> <!-- /row -->
+    </div> <!-- /container -->
+  </section>
+
+  <header>
+    <div class="container">
+      <div class="site-title">
+        <a href="http://goldsen.library.cornell.edu" class="secondary-brand">Rose Goldsen Archive of New Media Art</a>
+        <h1><a href="/etc2/">ETC / Experimental Television Center</a></h1>
+      </div>
+      <div class="navigation">
+        <div class="navbar">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <i class="fa fa-bars"></i> Menu
+          </button>
+          <div class="collapse navbar-collapse">
+            <div class="nav navbar-nav">
+              <ul class="menu nav"><li><a href="/etc/browse">Browse collection</a></li>
+                <li><a href="artists.html">Artists</a></li>
+                <li><a href="tools.html">Tools and Technology</a></li>
+                <li><a href="about.html">About ETC</a></li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
