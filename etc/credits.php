277e10ec2a99a299c70554d445966dbc78e02639
Melissa Wallace <mhk33@cornell.edu>
2017-03-22 10:42:35 -0400

    fix leftover drupal paths

--- etc/credits.php
+++ etc/credits.php
@@ -17,8 +17,8 @@
 
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
-    <script src="/sites/all/themes/etc3/javascripts/html5shiv.js"></script>
-    <script src="/sites/all/themes/etc3/javascripts/respond.min.js"></script>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
   <![endif]-->
 
 </head>
