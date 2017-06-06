001b681401c5496f4aec0b9111b06282dc0c9307
Melissa Wallace <mhk33@cornell.edu>
2017-06-06 10:55:20 -0400

    CULUX-832: switch to new piwik install

--- inc/piwik.php
+++ inc/piwik.php
@@ -1,16 +1,15 @@
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
-  _paq.push(["setDomains", ["*.goldsen.library.cornell.edu"]]);
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//webanalytics.library.cornell.edu/";
+    var u="//webstats.library.cornell.edu/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 561]);
+    _paq.push(['setSiteId', '32']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//webanalytics.library.cornell.edu/piwik.php?idsite=561" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
